f612167464381cabaaa93c96f3d871f006b7585b
Rafael Sousa <rafael.herculanosousa@gmail.com>
2017-10-07 23:06:33 -0300

    Tela de login, sem funcionalidades

---
+++ Rafael_herculano/app/src/main/java/br/com/rafaelsousa/rafael_herculano/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.rafaelsousa.rafael_herculano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
