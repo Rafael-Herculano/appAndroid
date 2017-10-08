a9bea231d65337fc15b3e68cdad6adc693645eec
Rafael Sousa <rafael.herculanosousa@gmail.com>
2017-10-08 15:40:13 -0300

    Adicionando ActionBar

--- Rafael_herculano/app/src/main/java/br/com/rafaelsousa/rafael_herculano/TelaInicial.java
+++ Rafael_herculano/app/src/main/java/br/com/rafaelsousa/rafael_herculano/TelaInicial.java
@@ -2,7 +2,11 @@ package br.com.rafaelsousa.rafael_herculano;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class TelaInicial extends DebugActivity {
 
@@ -12,5 +16,30 @@ public class TelaInicial extends DebugActivity {
         setContentView(R.layout.activity_tela_inicial);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_atualizar){
+            Toast.makeText(TelaInicial.this, "Atualizar", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_buscar){
+            Toast.makeText(TelaInicial.this, "Buscar", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_adicionar){
+            Toast.makeText(TelaInicial.this, "Adicionar", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_config){
+            Toast.makeText(TelaInicial.this, "Configurações", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_share){
+            Toast.makeText(TelaInicial.this, "Compartilhar", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
